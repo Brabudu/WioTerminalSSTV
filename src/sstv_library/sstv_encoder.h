0f1e50061e7dbf412244e0702950f1d8d7f790e1
Brabudu <32797053+Brabudu@users.noreply.github.com>
2025-09-12 16:15:09 +0200

    Reorganization of folders

---
+++ src/sstv_library/sstv_encoder.h
@@ -0,0 +1,49 @@
+//  _  ___  _   _____ _     _
+// / |/ _ \/ | |_   _| |__ (_)_ __   __ _ ___
+// | | | | | |   | | | '_ \| | '_ \ / _` / __|
+// | | |_| | |   | | | | | | | | | | (_| \__ \.
+// |_|\___/|_|   |_| |_| |_|_|_| |_|\__, |___/
+//                                  |___/
+//
+// Copyright (c) Jonathan P Dawson 2025
+// filename: sstv_encoder.h
+// description: class to encode sstv from image
+// License: MIT
+//
+
+#ifndef __SSTV_ENCODER_H__
+#define __SSTV_ENCODER_H__
+
+#include <cstdint>
+
+enum e_sstv_tx_mode {tx_martin_m1, tx_martin_m2, tx_scottie_s1, tx_scottie_s2, tx_PD_50, tx_PD_90, tx_PD_120, tx_PD_180};
+
+class c_sstv_encoder
+{
+
+  private :
+  double m_Fs_Hz;
+  uint32_t m_phase;
+  int16_t m_sin_table[1024];
+  uint32_t m_residue_f16;
+  void output_samples(uint32_t frequency, uint16_t samples);
+  void generate_tone(uint16_t frequency, uint32_t time_ms_f16);
+  bool calculate_parity(uint8_t number);
+  void generate_vis_bit(uint8_t level);
+  void generate_vis_code(e_sstv_tx_mode mode);
+  uint16_t get_pixel(uint16_t width, uint16_t height, uint16_t y, uint16_t x, uint8_t colour);
+  void generate_scottie(e_sstv_tx_mode mode);
+  void generate_martin(e_sstv_tx_mode mode);
+  void generate_pd(e_sstv_tx_mode mode);
+
+  //override these application specific functions
+  virtual void output_sample(int16_t sample) = 0;
+  virtual uint8_t get_image_pixel(uint16_t width, uint16_t height, uint16_t y, uint16_t x, uint8_t colour) = 0;
+
+  public:
+  c_sstv_encoder(double fs_Hz);
+  void generate_sstv(e_sstv_tx_mode);
+
+};
+
+#endif
